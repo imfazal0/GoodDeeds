061d4338ae2c94cda2d242e7bee6d6e528d85261
Fazal <fazalyt990@gmail.com>
2026-03-11 09:18:40 +0530

    Initialize The Project and Installed NativeWind for styling

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from "react-native"
+import { SafeAreaView  } from "react-native-safe-area-context"
+import "./global.css"
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView className='w-screen h-screen '>
+        <Text>Hello world</Text>
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
